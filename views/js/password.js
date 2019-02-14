4e40c350d59b6f3e6e0e66a02864c2c87581887e
LotuxPunk <vandendaelenclement@gmail.com>
2019-02-14 18:52:40 +0100

    pwd comments

--- views/js/password.js
+++ views/js/password.js
@@ -6,11 +6,6 @@ pBox.onchange = checkPass;
 pBox2.onchange = checkPass;
 
 function checkPass(){
-    // console.log(pBox.value.length >= 8 && pBox.value === pBox2.value);
-    // console.log(pBox.value);
-    // console.log(pBox2.value);
-    // console.log(pBox.value === pBox2.value);
-    // console.log(pBox.value.length >= 8);
     if(pBox.value.length >= 8 && pBox.value === pBox2.value){
         buttonReset.disabled = false;
         buttonReset.classList.remove("disabled");
