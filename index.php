725bd1c4f24bc2dd10dc3d198daec7cdc4b79b7f
LotuxPunk <vandendaelenclement@gmail.com>
2019-05-22 21:17:53 +0200

    fix bug with disconnection

--- index.php
+++ index.php
@@ -15,8 +15,9 @@
         if(isset($_SESSION['connected'])){
             if($_GET['p'] == "logout"){
                 session_destroy();
-                header("Refresh:0");
+                $_SESSION = array();
                 getHomePage();
+                header("Refresh:0");
             }
             elseif($_GET['p'] == "add-request"){
                 addRequest(htmlspecialchars($_POST['title']), htmlspecialchars($_POST['description']));
@@ -69,6 +70,7 @@
         }
         elseif($_GET['p'] == "check-insc"){
             checkInsc(htmlspecialchars($_POST['username']), htmlspecialchars($_POST['password']), htmlspecialchars($_POST['password2']), htmlspecialchars($_POST['email']));
+            // getHomePage("Momentarily unavailable");
         }
         elseif($_GET['p'] == "check-login"){
             checkConn(htmlspecialchars($_POST['password']), htmlspecialchars($_POST['email']));
