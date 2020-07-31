8b3a843cd122919b57e8bcfdbcc49a15f6cb4f4d
LotuxPunk <vandendaelenclement@gmail.com>
2020-07-31 21:12:41 +0200

    Cleaning LoginController

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -22,9 +22,7 @@ class LoginController extends AbstractController
      */
     public function index()
     {
-        return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
-        ]);
+        return $this->render('login/index.html.twig');
     }
 
     /**
@@ -34,7 +32,6 @@ class LoginController extends AbstractController
     {
         $url = $urlGeneratorInterface->generate('client', [], UrlGeneratorInterface::ABSOLUTE_URL);
         return new RedirectResponse("https://discord.com/api/oauth2/authorize?client_id=$this->discordClientId&redirect_uri=$url&response_type=code&scope=identify%20email");
-        //https://discord.com/api/oauth2/authorize?client_id=728616613120835625&redirect_uri=https%3A%2F%2Fpunkproject.xyz%2Fauth&response_type=code&scope=identify%20email
     }
 
     
