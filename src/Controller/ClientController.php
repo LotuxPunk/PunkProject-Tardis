6f9d449e0d599aca5c254d64bf2df9c4a0320d83
LotuxPunk <vandendaelenclement@gmail.com>
2020-07-31 21:34:43 +0200

    Bug fix and UserProfile infos

---
+++ src/Controller/ClientController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ClientController extends AbstractController
+{
+    /**
+     * @Route("/client", name="client")
+     */
+    public function index()
+    {
+        return new Response("");
+    }
+}
